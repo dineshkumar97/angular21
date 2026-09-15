6adea3d2fc3d98e3f5624ba933594b2f1eb3e357
dineshkumar97 <dineshkumarppn07@gmail.com>
2026-09-15 21:52:32 +0530

    common date

--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
-// import { DatePipe } from '@angular/common';
+
 import { UserService, User } from './user-service';
 import { isPlatformBrowser } from '@angular/common';
+import { CommonDatePipe } from '../../common/common-date.pipe';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonDatePipe],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
