5c69db0877167a313d6a562f0fe87effffa4df02
dineshkumar97 <dineshkumarppn07@gmail.com>
2026-09-15 22:18:14 +0530

    ui canges

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,10 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   RouterLink,
   RouterLinkActive,
   RouterOutlet
 } from '@angular/router';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  route: string;
+}
+
+interface Product {
+  sku: string;
+  name: string;
+  category: string;
+  stock: number;
+  reorderPoint: number;
+  status: 'Low Stock' | 'Out of Stock';
+}
 @Component({
   selector: 'app-main-layout',
   imports: [RouterLink, RouterLinkActive, RouterOutlet],
@@ -18,35 +33,158 @@ export class MainLayout implements OnInit {
   ngOnInit(): void { }
 
 
-  userName = 'Dinesh';
-  isDropdownOpen = false;
+  // userName = 'Dinesh';
+  isDropdownOpen = signal(false);
 
-  toggleDropdown(): void {
-    this.isDropdownOpen = !this.isDropdownOpen;
+  // toggleDropdown(): void {
+  //   this.isDropdownOpen = !this.isDropdownOpen;
+  // }
+
+  // profile(): void {
+  //   this.isDropdownOpen = false;
+
+  //   // Navigate to profile
+  //   // this.router.navigate(['/profile']);
+  // }
+
+  // settings(): void {
+  //   this.isDropdownOpen = false;
+
+  //   // Navigate to settings
+  //   // this.router.navigate(['/settings']);
+  // }
+
+
+  // logout(): void {
+  //   this.isDropdownOpen = false;
+
+  //   // Clear local storage
+  //   sessionStorage.clear();
+  //   // Redirect to login page
+  //   this.router.navigate(['/login']);
+
+  // }
+
+   userName = signal('Dinesh');
+
+  // isDropdownOpen = signal(false);
+
+  // ============================
+  // SIDEBAR
+  // ============================
+
+  menuItems = signal<MenuItem[]>([
+    {
+      label: 'Dashboard',
+      icon: '▦',
+      route: '/dashboard'
+    },
+    {
+      label: 'Products',
+      icon: '◇',
+      route: '/products'
+    },
+    {
+      label: 'Inventory',
+      icon: '▤',
+      route: '/inventory'
+    },
+    {
+      label: 'Orders',
+      icon: '▢',
+      route: '/orders'
+    },
+    {
+      label: 'Suppliers',
+      icon: '♧',
+      route: '/suppliers'
+    },
+    {
+      label: 'Reports',
+      icon: '▤',
+      route: '/reports'
+    },
+    {
+      label: 'Settings',
+      icon: '⚙',
+      route: '/settings'
+    }
+  ]);
+
+  activeMenu = signal('Dashboard');
+
+  // ============================
+  // DASHBOARD STATISTICS
+  // ============================
+
+ 
+
+  // ============================
+  // REVENUE DATA
+  // ============================
+
+  revenueData = signal([
+    { month: 'Jan', value: 43 },
+    { month: 'Feb', value: 38 },
+    { month: 'Mar', value: 52 },
+    { month: 'Apr', value: 48 },
+    { month: 'May', value: 57 },
+    { month: 'Jun', value: 63 },
+    { month: 'Jul', value: 60 },
+    { month: 'Aug', value: 67 },
+    { month: 'Sep', value: 72 },
+    { month: 'Oct', value: 69 },
+    { month: 'Nov', value: 76 },
+    { month: 'Dec', value: 86 }
+  ]);
+
+  maxRevenue = computed(() => {
+    return Math.max(
+      ...this.revenueData().map(item => item.value)
+    );
+  });
+
+  // ============================
+  // INVENTORY DATA
+  // ============================
+
+
+  // ============================
+  // MENU
+  // ============================
+
+  selectMenu(menu: MenuItem) {
+    this.activeMenu.set(menu.label);
   }
 
-  profile(): void {
-    this.isDropdownOpen = false;
+  // ============================
+  // PROFILE
+  // ============================
 
-    // Navigate to profile
-    // this.router.navigate(['/profile']);
+  toggleDropdown() {
+    this.isDropdownOpen.update(value => !value);
   }
 
-  settings(): void {
-    this.isDropdownOpen = false;
-
-    // Navigate to settings
-    // this.router.navigate(['/settings']);
+  profile() {
+    console.log('Profile clicked');
+    this.isDropdownOpen.set(false);
   }
 
+  settings() {
+    console.log('Settings clicked');
+    this.isDropdownOpen.set(false);
+  }
 
-  logout(): void {
-    this.isDropdownOpen = false;
+  logout() {
+    console.log('Logout clicked');
+    this.isDropdownOpen.set(false);
+  }
 
-    // Clear local storage
-    sessionStorage.clear();
-    // Redirect to login page
-    this.router.navigate(['/login']);
+  // ============================
+  // REORDER
+  // ============================
 
+  reorder(product: Product) {
+    console.log('Reorder:', product);
   }
 }
