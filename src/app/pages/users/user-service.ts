8b316adfc8969f0915695ceca29a665a4635ec39
dineshkumar97 <dineshkumarppn07@gmail.com>
2026-09-16 09:20:33 +0530

    environmnet added

--- src/app/pages/users/user-service.ts
+++ src/app/pages/users/user-service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export interface User {
   _id: string;
@@ -15,11 +16,12 @@ export interface User {
 })
 export class UserService {
 
-  private apiUrl = 'http://localhost:3000/api-learn/user/all';
+  // private apiUrl = 'http://localhost:3000/api-learn/user/all';
+  private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    return this.http.get<User[]>(`${this.apiUrl}/user/all`);
   }
 }
